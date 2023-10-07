a6715f0eb92492c13746a7038edd72ff028b4a0b
Benedict <benumeozor@gmail.com>
2023-10-07 06:23:17 +0100

    added signin and sigup pages

---
+++ app/login/page.tsx
@@ -0,0 +1,48 @@
+import styles from "./page.module.scss";
+import image from "@/assets/signin.svg";
+import Image from "next/image";
+import google from "@/assets/google.png";
+import Link from "next/link";
+import Container from "@/components/Layouts/Container";
+
+const Page = () => {
+  return (
+    <main className={styles.page}>
+      <Container className={styles.container}>
+        <div className={styles.left}>
+          <Image src={image} alt="register" height={500} width={500} />
+        </div>
+        <div className={styles.right}>
+          <h2>Welcome Back</h2>
+          <p className={styles.intro}>
+            Explore new Opportunities at <Link href={"/"}>BAUMco</Link>
+          </p>
+          <form className="auth-form">
+            <button className={`${styles.google}`}>
+              <Image src={google} alt="google" height={30} width={30} />
+              Sign in with google
+            </button>
+            <div className={styles.or}>OR</div>
+            <div className="form-field">
+              <label htmlFor="email">Email*</label>
+              <input type="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="form-field">
+              <label htmlFor="password">Password*</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button className="submit">Signin</button>
+          </form>
+          <p className="redirect">
+            Already have an account? <Link href="/register">Signup</Link>
+          </p>
+        </div>
+      </Container>
+    </main>
+  );
+};
+export default Page;
