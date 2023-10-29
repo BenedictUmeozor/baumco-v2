bbef953d58af2fcb2e4da1614ccbc52a922a0d7b
Benedict <benedictumeozor@gmail.com>
2023-10-29 13:53:35 +0000

    fixing react quill errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Hero from "@/components/Hero";
 import styles from "./page.module.scss";
 import Posts from "@/components/Posts";
 import CategorySection from "@/components/Category";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Baumco web community | Home",
+  description:
+    "Elevate your tech prowess with BAUMco. Immerse in programming insights, latest trends, and tutorials. Empower your journey in the world of tech and programming excellence.",
+};
 
 export default function Home() {
   return (
